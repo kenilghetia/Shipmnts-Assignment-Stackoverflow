4e3b068bfb7304950ab8492ee46c96984538a45e
Kenil Ghetia <kenil.ghetia@gmail.com>
2023-08-21 13:37:50 +0530

    add modified readme

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 ### Backend Option 1: StackOverFlow Clone
 ### List of APIs needed:
-Create User: A user can register himself on the system
-Create Questions: User can create a Questions
-Update Questions: User can update the Questions
-Delete Questions: User can delete the Questions
-All Questions: List of the all questions 
-Votes: Implementing a voting system (upvotes/downvotes) for questions and answers.
-Comments: Commenting system for questions and answers.
+Create User: A user can register himself on the system,
+Create Questions: User can create a Questions,
+Update Questions: User can update the Questions,
+Delete Questions: User can delete the Questions,
+All Questions: List of the all questions,
+Votes: Implementing a voting system (upvotes/downvotes) for questions and answers,
+Comments: Commenting system for questions and answers,
 Bonus: Search, Sorting questions
 
-
-
-# Project Documentation:
-1. MVC Structure is followed for writing quality code
+## Project Documentation:
+1. MVC Structure is followed for writing quality code.
+We have various directories. config folder contains configuration files.
+middlewares
+2. We have 3 models one for user details, one for all questions that contains all answers in form of an array and one for storing all answers.
+3. Once user register using email and password, user details are stored in user_details where password is stored in encrypted form.
+4. user can then register using email and password, a JWT token is generated to keep track of user
+5. further we have created routes for each feature required for our app and dedicated api functions are created in controllers folder which uses various models described in models folder
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,42 +1,42 @@
 const express = require("express");
 const userControllers = require("../controllers/userControllers.js");
-
+const JWTVerification = require('../middlewares/jwtAuth.js');
 const router = express.Router();
 
 // Routes for Users
 router.post("/create", userControllers.createUser);
 router.post("/login", userControllers.loginUser);
-router.post("/addquestion", userControllers.addMovie);
-router.get("/getall", userControllers.getAllQuestions);
+router.post("/addquestion", JWTVerification, userControllers.addMovie);
+router.get("/getall", JWTVerification,userControllers.getAllQuestions);
 
-router.put("/update/:questionId", userControllers.updateQuestion);
-router.delete("/delete/:questionId", userControllers.deleteQuestion);
+router.put("/update/:questionId", JWTVerification, userControllers.updateQuestion);
+router.delete("/delete/:questionId", JWTVerification, userControllers.deleteQuestion);
 
 // Upvote a question
-app.put("/questions/:questionId/upvote", userControllers.upvote);
+app.put("/questions/:questionId/upvote", JWTVerification, userControllers.upvote);
 
 // Downvote a question
-app.put("/questions/:questionId/downvote", userControllers.downvote);
+app.put("/questions/:questionId/downvote", JWTVerification, userControllers.downvote);
 
 // Upvote an answer
-app.put("/answers/:answerId/upvote", userControllers.upvote);
+app.put("/answers/:answerId/upvote", JWTVerification, userControllers.upvote);
 
 // Downvote an answer
-app.put("/answers/:answerId/downvote", userControllers.downvote);
+app.put("/answers/:answerId/downvote", JWTVerification, userControllers.downvote);
 
 // Add a comment to a question
-app.post("/questions/:questionId/comments", userControllers.createComment);
+app.post("/questions/:questionId/comments", JWTVerification, userControllers.createComment);
 
 // Add a comment to an answer
-app.post("/answers/:answerId/comments", userControllers.createAnswer);
+app.post("/answers/:answerId/comments", JWTVerification, userControllers.createAnswer);
 
 // Search questions by keyword
-app.get("/questions/search", (req, res) => {
+app.get("/questions/search", JWTVerification, (req, res) => {
   const keyword = req.query.keyword;
 });
 
 // Sort questions by criteria (e.g., most upvoted)
-app.get("/questions/sort", (req, res) => {
+app.get("/questions/sort", JWTVerification, (req, res) => {
   const sortBy = req.query.sortBy;
 });
 
